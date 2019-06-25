cba61bbea9001eb19631d5cd3eaf85bc810d2fc9
Patrick Harrison <jahman@bellatlantic.net>
2019-06-25 22:10:35 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,8 +5,8 @@ end
 def deal_card
  20.times do 
       card = deal_card
-      expect(card).to be > 0
-      expect(card).to be < 12
+      card.to be > 0
+      card.to be < 12
 
 def display_card_total
   # code #display_card_total here
