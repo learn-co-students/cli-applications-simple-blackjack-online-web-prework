c9be3c452e060cd8386648d81e58efae5ccc3d46
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 19:46:31 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,36 @@
-def welcome
-  # code #welcome here
+expect($stdout) .to recieve(:puts).with("Welcome to the Black jack Table")
 end
-
-def deal_card
-  # code #deal_card here
+expect(card) .to be > 0 
 end
-
-def display_card_total
-  # code #display_card_total here
+expect { display_card_total(7) } .to_not raise_error
 end
-
-def prompt_user
-  # code #prompt_user here
+#display_card_total prints the value of the cards to the screen
 end
-
-def get_user_input
-  # code #get_user_input here
+#display_card_total does not hardcode the card total
 end
-
-def end_game
-  # code #end_game here
+expect ($stdout) .to recieve(:puts).with("Type 'h' to hit or 's' to stay")
 end
-
-def initial_round
-  # code #initial_round here
+expect (get_user_input) .to eq(string)
 end
-
-def hit?
-  # code hit? here
+#end_game prints apology, card total, and thank you message 
 end
-
-def invalid_command
-  # code invalid_command here
+expect(initial_round) .to eq(12)
 end
-
+expect(self) .to recieve(:deal_card) .at_least(:twice).and_return(6)
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
-
-def runner
-  # code runner here
+expect($stdout) .to recieve(:puts).with("Please enter a vlid command")
 end
-    
+#hit? calls on #prompt_user then #get_user_input
+end
+#hit? returns an integer which is the card total 
+end 
+#hit? does not deal another card if user types 's'
+end
+#hit? deals another card when user types 'h'
+end 
+#hit displays the invalid command message and prompts the user again if a user inputs something other than 'h' or 's' 
+end
+expect(self) .to recieve(:deal_card).at_least(3).times.and_return(10)
+end
