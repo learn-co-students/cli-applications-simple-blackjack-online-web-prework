0af6ead572a780a6abde1d9bc3e8aa4b2d3d88f8
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-07 20:45:15 +0000

    Automatically backed up by Learn

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -17,12 +17,12 @@ end
 
 describe "#display_card_total" do
   it "accepts one argument, the card total" do
-    expect { display_card_total(7) }.to_not raise_error
+    expect { display_card_total('7') }.to_not raise_error
   end
 
   it "prints the value of the cards to the screen" do
-    expect($stdout).to receive(:puts).with("Your cards add up to 8")
-    display_card_total(8)
+    expect("$stdout").to receive(":puts").with("Your cards add up to 8")
+    display_card_total('8')
   end
 
   it "does not hardcode the card total" do
