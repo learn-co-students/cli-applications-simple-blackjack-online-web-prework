48ff5da68f80cde562928be2da64c9cf9a1c53cd
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-07 20:44:25 +0000

    Automatically backed up by Learn

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -1,6 +1,6 @@
 describe "#welcome" do
   it "prints a welcome message to screen" do
-    expect($stdout).to receive(":puts").with("Welcome to the Blackjack Table")
+    expect('$stdout').to receive(":puts").with("Welcome to the Blackjack Table")
     welcome
   end
 end
