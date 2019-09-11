2c09b983b59f5f0fd92d1c0a3e8b4f923ff8fa67
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-11 20:57:47 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,52 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand 1..11
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+  puts "Your cards add up to #{num}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  play = gets.chomp
+  play
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  sum
 end
 
 def invalid_command
-  # code invalid_command here
+  puts 'Please enter a valid command'
+end
+
+def hit?(num)
+  prompt_user
+  play = get_user_input
+  if play == 's'
+    num
+  elsif play == 'h'
+    deal_card + num
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+  end
 end
 
 #####################################################
@@ -39,6 +54,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  init_total = initial_round
+  until init_total > 21
+    init_total = hit?(init_total)
+    display_card_total(init_total)
+    if init_total > 21
+      end_game(init_total)
+      break
+    end
+  end
 end
     
