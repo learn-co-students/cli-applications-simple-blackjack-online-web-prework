8d9f6d14c36961b65c74ee7bdf301aec7ae626f8
Prince Charles <princecharles35@yahoo.com>
2019-09-26 13:18:35 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,10 @@
-def welcome
-  # code #welcome here
-end
-
-def deal_card
-  # code #deal_card here
-end
-
-def display_card_total
-  # code #display_card_total here
-end
-
-def prompt_user
-  # code #prompt_user here
-end
-
-def get_user_input
-  # code #get_user_input here
-end
-
-def end_game
-  # code #end_game here
-end
-
-def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
-end
-
-def invalid_command
-  # code invalid_command here
-end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
-def runner
-  # code runner here
-end
-    
+describe "#display_card_total" do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
+ 
+  it "prints the value of the cards to the screen" do
+    expect($stdout).to receive(:puts).with("Your cards add up to 8")
+    display_card_total(8)
+  end
+  ....
