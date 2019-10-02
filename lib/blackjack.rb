bc1c6193f5b5b00d119e853301157a9c3ce2f892
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-02 17:52:11 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,54 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}" 
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay" 
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!" # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
+  first_card = deal_card
+  second_card = deal_card
+  total = first_card + second_card
+  display_card_total(total) # code #initial_round here
+  return total
 end
 
-def hit?
-  # code hit? here
+def hit?(current_total)
+  prompt_user
+  user_input = get_user_input
+  
+  while !(user_input == "h" || user_input == "s") do
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  
+  if (user_input == "h")
+    next_card = deal_card
+    current_total += next_card
+  end
+  current_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,18 @@
 require_relative "blackjack.rb"
+require "pry"
+  
 
-runner
+def runner
+  welcome
+  total = initial_round
+  
+  do 
+    binding.pry
+    hit?(total)
+    display_card_total(total)
+    
+  until total > 21 
+  end_game(total)
+end
+
+ 
